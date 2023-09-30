ede9b065c0b97c35dac878735d3f4b62e80b0d5a
aaparky <aa.parky@gmail.com>
2023-09-30 10:00:44 +0100

    comments and tidy

--- test.php
+++ test.php
@@ -4,46 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!-- Prevent caching -->
+    <!-- Meta tags to prevent browser caching -->
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
     <meta http-equiv="Expires" content="0" />
 
+    <!-- Title for the webpage -->
     <title>Mrglglglgl.com Read it before you are in it!</title>
     
-<!-- importing html2canvas -->
+    <!-- Importing html2canvas library to capture page content as images -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.min.js"></script>
 
-    <!-- save button script -->
+    <!-- Scripts to handle the "save" and "refresh" button functionalities -->
     <script src="scripts/save-button.js"></script>
-    <!-- refresh button script -->
     <script src="scripts/refresh-button.js"></script>
 
-<!-- Importing the Google Font -->
-   <link rel="preconnect" href="https://fonts.googleapis.com">
-   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-   <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
-   <link rel="stylesheet" href="https://use.typekit.net/tkq4iko.css">
-<!-- My stylesheet -->
-   <link rel="stylesheet" href="css/test.css">
+    <!-- Importing Google Fonts and another external stylesheet -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+    <!-- Adobe fonts -->
+    <link rel="stylesheet" href="https://use.typekit.net/tkq4iko.css">
+
+    <!-- Link to the site's main stylesheet -->
+    <link rel="stylesheet" href="css/test.css">
 </head>
 
 <body>
     
-    <!-- Hero Image -->
+    <!-- Container for the main image content -->
     <div class="image-container" id="captureContent">
 
-        <!-- Headline -->
+        <!-- Main headline for the webpage -->
         <div class="headline">
             Suddenly...
         </div>
 
-        <!-- Sub Headline -->
+        <!-- Sub-headline generated using a PHP script -->
         <?php include 'scripts/sub-headline.php'; ?>
         <div class="sub-headline">
         <?php echo generateSubHeadline(); ?>
         </div>
-        <!-- Bubbles -->
+
+        <!-- Speech bubbles with content fetched using a PHP script -->
         <?php include 'scripts/parse_master_alliance.php'; ?>
         <div class="electric electric-bottom-left">
             <?php echo $greeting; ?>!
@@ -53,24 +56,22 @@
             <?php echo $lastWords; ?> 
         </div>
         
+        <!-- Main hero image for the site -->
         <img src="img/landing00.png" alt="The Mr. Mrglglglgl">
 
-         <!-- Slug-Line -->
+        <!-- Slug-line generated using a PHP script -->
         <?php include 'scripts/slug-line.php'; ?>
         <div class="slug-line">
         <?php echo generateSlugLine(); ?>
         </div>
 
     </div>
-    
-    
-</body>
 
-<div class="button-container">
-    <!-- Add this button anywhere within your <body> tag -->
+    <!-- Container for the "save" and "refresh" buttons -->
+    <div class="button-container">
         <button id="saveAsPngButton" class="save-button">Save as PNG</button>
         <button id="refreshButton" onclick="refreshPage()">Refresh</button>
+    </div>
 
-</div>
-
+</body>
 </html>
